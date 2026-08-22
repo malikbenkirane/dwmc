6dc655f8eb86755584b96265a35414f44dfcf4a6
Malik Benkirane <mbenkirane@internetworks.blog>
2026-08-22 10:41:12 +0100

    build: wire tools image into core via multi-stage
    core now COPYs crush and jj from dwmc:tools, so build.sh runs the tools
    stage first to satisfy the dependency.

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-container}"
 
+"$CONTAINER_RUNTIME" build -t dwmc:tools ./tools
 "$CONTAINER_RUNTIME" build -t dwmc:bookworm .
 "$CONTAINER_RUNTIME" build -t dwmc:core ./core
-"$CONTAINER_RUNTIME" build -t dwmc:tools ./tools
