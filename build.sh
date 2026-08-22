8bfe7e8170370bcb3649e9ea0bff856088d781d5
Malik Benkirane <mbenkirane@internetworks.blog>
2026-08-22 16:48:39 +0100

    build: add golang image with LSP tooling
    Provides gopls, golangci-lint-langserver, and dlv on top of dwmc:core so
    Go development can happen fully in-container without installing language
    servers on the host.

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-container}"
 "$CONTAINER_RUNTIME" build -t dwmc:tools ./tools
 "$CONTAINER_RUNTIME" build -t dwmc:bookworm .
 "$CONTAINER_RUNTIME" build -t dwmc:core ./core
+"$CONTAINER_RUNTIME" build -t dwmc:golang ./golang
